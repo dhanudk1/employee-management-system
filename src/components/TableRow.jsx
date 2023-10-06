bc54d262748956e71f502354db6a8f1e9fe5ee21
dhanudk1 <kuted518@gmail.com>
2023-10-06 06:30:41 -0700

    some small changes in classes

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -4,28 +4,26 @@ const TableRow = (props) => {
   const { user, loadUser, deleteUser } = props;
 
   return (
-    <div className="table-responsive">
-      <tr>
-        <td>{user.id}</td>
-        <td>{user.name}</td>
-        <td>{user.email}</td>
-        <td>{user.city}</td>
-        <td>
-          <button
-            className="btn btn-danger"
-            onClick={() => deleteUser(user.id)}
-          >
-            <i className="fa fa-trash"></i>
-          </button>
-          <button
-            className="btn btn-primary ms-2"
-            onClick={() => loadUser(user)}
-          >
-            <i className="fa fa-pencil"></i>
-          </button>
-        </td>
-      </tr>
-    </div>
+    <tr>
+      <td>{user.id}</td>
+      <td>{user.name}</td>
+      <td>{user.email}</td>
+      <td>{user.city}</td>
+      <td>
+        <button
+          className="btn btn-danger me-2"
+          onClick={() => deleteUser(user.id)}
+        >
+          <i className="fa fa-trash"></i>
+        </button>
+        <button
+          className="btn btn-primary mt-sm-0 mt-2"
+          onClick={() => loadUser(user)}
+        >
+          <i className="fa fa-pencil"></i>
+        </button>
+      </td>
+    </tr>
   );
 };
 
