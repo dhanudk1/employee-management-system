e83caf34d3830c63a29738eafef1de08da402df8
dhanudk1 <kuted518@gmail.com>
2023-10-06 05:49:32 -0700

    changed table

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -4,20 +4,28 @@ const TableRow = (props) => {
   const { user, loadUser, deleteUser } = props;
 
   return (
-    <tr>
-      <td>{user.id}</td>
-      <td>{user.name}</td>
-      <td>{user.email}</td>
-      <td>{user.city}</td>
-      <td>
-        <button className="btn btn-danger" onClick={() => deleteUser(user.id)}>
-          <i className="fa fa-trash"></i>
-        </button>
-        <button className="btn btn-primary ms-2" onClick={() => loadUser(user)}>
-          <i className="fa fa-pencil"></i>
-        </button>
-      </td>
-    </tr>
+    <div className="table-responsive">
+      <tr>
+        <td>{user.id}</td>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        <td>{user.city}</td>
+        <td>
+          <button
+            className="btn btn-danger"
+            onClick={() => deleteUser(user.id)}
+          >
+            <i className="fa fa-trash"></i>
+          </button>
+          <button
+            className="btn btn-primary ms-2"
+            onClick={() => loadUser(user)}
+          >
+            <i className="fa fa-pencil"></i>
+          </button>
+        </td>
+      </tr>
+    </div>
   );
 };
 
